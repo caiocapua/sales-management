9f4f3b37c5d2789eb838349c731a3b0b6add0c6d
caiocapua <caiocapua@hotmail.com>
2022-09-14 16:57:02 -0300

    feat: create a connection for typeorm and database

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,15 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import { AppError } from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
-
 app.use(
     (
         error: Error,
