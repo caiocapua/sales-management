48f9b98b4ba37636b850c911e04556ddfb12ef6f
caiocapua <caiocapua@hotmail.com>
2022-09-15 09:20:45 -0300

    feat: create a routes of products

---
+++ src/modules/products/routes/products.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import ProductsController from '../controllers/ProductsController';
+
+const productsRouter = Router();
+const productsController = new ProductsController();
+
+productsRouter.get('/', productsController.list);
+productsRouter.get('/:id', productsController.show);
+productsRouter.post('/', productsController.create);
+productsRouter.put('/:id', productsController.update);
+productsRouter.delete('/:id', productsController.remove);
+
+export default productsRouter;
