bbd13fac4e72374f9ed65b3f2fdcc11c17a54ad9
caiocapua <caiocapua@hotmail.com>
2022-09-15 09:38:03 -0300

    feat: create a migration of products

---
+++ src/shared/database/typeorm/migrations/1663185802300-CreateProducts.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateProducts1663185802300 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'tb_products',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'price',
+                        type: 'decimal',
+                        precision: 10,
+                        scale: 2,
+                    },
+                    {
+                        name: 'quantity',
+                        type: 'int',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: null,
+                        isNullable: true,
+                    },
+                    {
+                        name: 'deleted_at',
+                        type: 'timestamp',
+                        default: null,
+                        isNullable: true,
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('tb_products');
+    }
+}
